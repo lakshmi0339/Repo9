1053c3fcd1ad6ea775862ff4c8b66f92a9eb16f9
Lakshmi0339 <laxmijntu1989@gmail.com>
2023-11-10 11:31:40 +0530

    commit

---
+++ GitHubPro/src/main/java/GitHubPro/GitHubPro/App.java
@@ -0,0 +1,13 @@
+package GitHubPro.GitHubPro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
